70f0ede441b54182f6055da2b5741505f8231ec6
Vicki Bealman <drbealman@gmail.com>
2021-02-13 15:44:16 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// eslint-disable-next-line no-unused-vars
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from Dr. Vicki</h1>
+        <h1>We now have Auth!</h1>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
